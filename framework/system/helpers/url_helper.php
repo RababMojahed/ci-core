6cc5700cb7fee3ff419fad8336ce526e9d201d29
Vu Thanh Tai <thanhtai96nd@gmail.com>
2017-09-19 08:05:01 +0700

    add public_url function

--- framework/system/helpers/url_helper.php
+++ framework/system/helpers/url_helper.php
@@ -87,6 +87,26 @@ if ( ! function_exists('base_url'))
 		return get_instance()->config->base_url($uri, $protocol);
 	}
 }
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('public_url'))
+{
+	/**
+	 * Base URL
+	 *
+	 * Create a local URL based on your basepath.
+	 * Segments can be passed in as a string or an array, same as site_url
+	 * or a URL to a file can be passed in, e.g. to an image file.
+	 *
+	 * @param	string	$uri
+	 * @param	string	$protocol
+	 * @return	string
+	 */
+	function public_url($uri = '', $protocol = NULL)
+	{
+		return base_url('public/' . $uri, $protocol);
+	}
+}
 
 // ------------------------------------------------------------------------
 
